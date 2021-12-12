4da795540d9c5552bea096cac8991de06fcaf106
Nate <natesvoboda64@gmail.com>
2021-12-12 14:40:05 -0500

    adding in characters initially

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,6 +4,10 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+    var length=(8,128);
+    var numeric = "0123456789";
+    var string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var speical = "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
