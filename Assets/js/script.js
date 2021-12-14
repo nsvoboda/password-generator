909e212a80b8c7f4c43e3d695c182eab829fbe50
Nate <natesvoboda64@gmail.com>
2021-12-14 11:57:21 -0500

    function with window prompts and validation for password length

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,10 +1,39 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Function that has prompts asking about password length and validates user input to be a number between 8 and 128.
+var setPassLength = function(){
+
+  passLength = window.prompt("How long should your password be? Please enter a number equal to OR between 8 and 128", "Type your password length here");
+
+  if (passLength >= 8 && passLength <= 128){
+    window.alert("Your password will be " + passLength + " characters long!");
+  }
+  else if (passLength < 8 || passLength > 128){
+    window.alert("The number you have entered is not within the valid range of 8 to 128, please try again!");
+    setPassLength();
+  }
+  else if (isNaN(!passLength)){
+    window.alert("Please enter a valid number between 8 and 128!");
+    setPassLength();
+  }
+  else{
+    window.alert("Please enter a valid number between 8 and 128!");
+    setPassLength();
+  }
+}
+
+
+
 // Write password to the #password input
 function writePassword() {
+  setPassLength();
+  
+    
+
+
+
   var password = generatePassword();
-    var length=(8,128);
     var numeric = "0123456789";
     var string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var speical = "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
